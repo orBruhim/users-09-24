8ff73979cc795009361d83a5407a565ddbe5f4ee
orBruhim <orbruhim@gmail.com>
2024-09-27 23:35:35 +0300

    add users table

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,13 +1,27 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {UsersService} from "./users.service";
+import {AsyncPipe, DatePipe, TitleCasePipe} from "@angular/common";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [
+    TitleCasePipe,
+    AsyncPipe,
+    DatePipe
+  ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UsersComponent {
+  private usersService= inject(UsersService);
+  private router= inject(Router);
+  users$= this.usersService.getUsers();
 
+
+  navigateToAddUser(id: number) :void  {
+    this.router.navigate(['/users', id])
+  }
 }
