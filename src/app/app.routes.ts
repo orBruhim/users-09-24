880dac2f4008b5670afd492ffd74e05b28943063
orBruhim <orbruhim@gmail.com>
2024-09-27 15:33:47 +0300

    initial app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'users',
+        loadChildren: () =>
+            import('./users/users.module').then(({ UsersModule }) => UsersModule),
+    },
+    { path: '**', redirectTo: '/users', pathMatch: 'full' },
+];
